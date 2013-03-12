865f1a12638ae482756af1b2a5350e468c702cd1
Nicholas Crandall <ncrandall@gmail.com>
2013-03-12 11:54:58 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Good Cal
+========
+
+This is the last calendar you will ever need to use. It syncs with all major
+calendars.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodcal::Application.config.secret_token = '97e992ac3a7492de963431d491274ed3752770c045e9022b92e5e3b90c9d1f0e5781fc5b4dd74497a2a977ca59a589bd31c7c8857d10d6917b232fe8c6e291a0'
